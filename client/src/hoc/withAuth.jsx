fb6cba643db13a47be8c93ed22c3c01649c090e7
Ignacio <nachodiaz99@gmail.com>
2022-08-14 18:45:03 -0400

    move up nabvar in component tree

--- client/src/hoc/withAuth.jsx
+++ client/src/hoc/withAuth.jsx
@@ -3,10 +3,12 @@ import { Navigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import { Container } from '@mui/system';
 import Sidebar from '../layouts/Sidebar';
+import Navbar from '../components/Navbar';
 
 function WithAuth({ children }) {
   return Cookies.get('jwt') !== undefined ? (
     <Container maxWidth={'xl'} disableGutters sx={{ paddingLeft: 10 }}>
+      <Navbar />
       <Sidebar />
       {children}
     </Container>
--- client/src/pages/Dashboard/index.jsx
+++ client/src/pages/Dashboard/index.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Grid } from '@mui/material';
-import Navbar from '../../components/Navbar';
 import HistoricalPriceChart from '../../components/HistoricalPriceChart';
 import StockChart from '../../components/StockChart';
 
 function Dashboard() {
   return (
     <>
-      <Navbar />
       <Grid container spacing={2} sx={{ padding: 2 }}>
         <Grid item xs={6}>
           <HistoricalPriceChart />
