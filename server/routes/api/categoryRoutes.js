b9ecc857dbe70ce5fa8d12d39a5637d77d6907e1
Ignacio <nachodiaz99@gmail.com>
2022-08-15 14:23:32 -0400

    update category route

--- server/routes/api/categoryRoutes.js
+++ server/routes/api/categoryRoutes.js
@@ -8,7 +8,7 @@ router.get('/', authMiddleware, async (req, res) => {
   try {
     const categories = await Category.find();
 
-    res.status(200).json(categories);
+    res.status(200).json({ total: categories.length, categories });
   } catch (err) {
     res.status(500).json(err);
   }
