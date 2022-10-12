395a1c5e715ccf4b2dcfefb5f1a8f971ec083e71
Ignacio <nachodiaz99@gmail.com>
2022-10-12 15:57:14 -0300

    Integrate Date Pickers

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from '@mui/material';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import { theme } from './assets/theme/index';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Login from './pages/Login';
@@ -9,20 +11,22 @@ import routes from './routes/routes';
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<PrivateRoutes />}>
-            {routes.map((element, index) => (
-              <Route
-                key={index}
-                path={element.path}
-                element={React.createElement(element.Component)}
-              />
-            ))}
-          </Route>
-          <Route path='/login' element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      <LocalizationProvider dateAdapter={AdapterMoment}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<PrivateRoutes />}>
+              {routes.map((element, index) => (
+                <Route
+                  key={index}
+                  path={element.path}
+                  element={React.createElement(element.Component)}
+                />
+              ))}
+            </Route>
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </LocalizationProvider>
     </ThemeProvider>
   );
 }
--- client/src/pages/FinanceTable/index.jsx
+++ client/src/pages/FinanceTable/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import moment from 'moment';
+import moment from 'moment';
 import {
   TableContainer,
   Table,
@@ -8,11 +8,14 @@ import {
   TableCell,
   TableRow,
   Input,
+  TextField,
 } from '@mui/material';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import Navbar from '../../layouts/Navbar';
 
 function FinanceTable() {
   const [inputValue, setInputValue] = React.useState('');
+  const [value, setValue] = React.useState(moment());
 
   function handleInputChange(event) {
     setInputValue(event.target.value);
@@ -21,6 +24,17 @@ function FinanceTable() {
   return (
     <>
       <Navbar title='Finance Table' />
+      <DatePicker
+        label='Basic example'
+        views={['month', 'year']}
+        minDate={moment().subtract(2, 'year')}
+        value={value}
+        onChange={newValue => {
+          setValue(newValue);
+        }}
+        renderInput={params => <TextField {...params} />}
+      />
+      <div>{value.format('MMM YYYY')}</div>
       <TableContainer>
         <Table>
           <TableHead>
@@ -36,9 +50,6 @@ function FinanceTable() {
           <TableBody>
             <TableRow>
               <TableCell>{Number(inputValue) + 100}</TableCell>
-              <TableCell>{Number(inputValue) + 200}</TableCell>
-              <TableCell>{Number(inputValue) + 300}</TableCell>
-              {/* <TableCell>{moment().week()}</TableCell> */}
             </TableRow>
           </TableBody>
         </Table>
